0cd31e10fa415f43ce6abe29eac52e27b913355e
Gleb Posobin <posobin@gmail.com>
2021-09-21 23:12:50 -0400

    Update the update and hello pages

--- src/main/ampie/pages/hello.cljs
+++ src/main/ampie/pages/hello.cljs
@@ -9,87 +9,42 @@
 
 (def state (r/atom {}))
 
+(defn subheader [text]
+  [:h3.pt-4.pb-2.text-2xl text])
+
+(defn highlight [text bad?]
+  [:span.rounded.pl-0dot5.pr-0dot5
+   {:class (if bad? :bg-indigo-200 :bg-yellow-200)}
+   text])
+
 (defn hello-page []
-  [:div.hello-page.content
-   [:div.header [:h1 "Hello from ampie!"]]
-   [:div.hello-container
-    (if-not @@user-info
+  [:div.font-sans.text-lg.leading-snug.ml-auto.mr-auto.w-fit-content.p-4.rounded-xl.shadow-xl.m-4.border.relative
+   [:h1.text-2xl.font-italic.pb-3 "Ampie installed"]
+   [:div.max-w-md
+    (cond
+      (not @@user-info)
       [:p "First, please sign up at "
        [:a (b/ahref-opts "https://ampie.app/register") "ampie.app"] "."]
-      [:p "Give ampie ten minutes to download and unpack its caches, "
-       "it might be CPU-intensive for 10 minutes while unpacking."])
-    [:p
-     "Once ampie downloads its caches, on some webpages you will see this bar:"
-     [:img.mini-tags {:src (.. browser -runtime (getURL "assets/images/mini-tags.png"))}]
-     "The numbers indicate the following:"]
-    [:ul
-     #_[:li [:div.ampie-mini-tag-icon.ampie-history-icon] " " [:b "Seen at"]
-        ": the number of times "
-        "you have seen the page you are on referenced on other pages you have been to. "]
-     [:li [:div.ampie-mini-tag-icon.ampie-visits-icon] " " [:b "Amplified"]
-      ": how many times this page has been “amplified” (shared with ampie) "
-      "by people you follow."]
-     [:li [:div.ampie-mini-tag-icon.ampie-twitter-icon] " " [:b "Tweets"]
-      ": the number of people you follow who have tweeted about this page."]
-     [:li [:div.ampie-mini-tag-icon.ampie-hn-icon] " " [:b "HN stories"]
-      ": the total number of comments the page has gotten on "
-      [:a (b/ahref-opts "https://news.ycombinator.com") "Hacker News"]]
-     [:li [:div.ampie-mini-tag-icon.domain-links-icon] " " [:b "Links on this domain"]
-      ": the number of pages on the same domain that have appeared in your "
-      "twitter feed, on hacker news or that someone you follow has amplified with ampie."]]
-    [:p "Click the bar to get more details about the page and the domain. "
-     "Keep in mind that this sends a request to ampie's server with the current URL."]
+      (not (:twitter-username @@user-info))
+      [:p.border.border-2.border-red-50.rounded-md.p-2 "Log in with twitter at "
+       [:a (b/ahref-opts "https://ampie.app/") "ampie.app"]
+       " to see tweets about pages you visit. Then read on."]
+      :else
+      [:p "Hi! Two things."])
+    [subheader "Sidebar"]
+    [:p "Easy: go to a page, sidebar pops up. "
+     "Not to annoy you, it " [highlight "pops up at most twice per page" false] ". "
+     "Use keyboard shortcuts, Luke: Alt-Alt, Shift-Shift."]
 
-    [:h3 "What about privacy?"]
-    [:p "Your browsing history is not sent anywhere. "
-     "The extension sends the URL of the page you are on to the server "
-     "only when you click the bar in the bottom right corner of the page, "
-     "click an ampie ampersand near a link, or amplify the page."]
+    [subheader "Leave feedback"]
+    [:p [highlight "Scroll to the bottom of the sidebar" false]
+     " to quickly leave any kind of feedback "
+     "(questions? ideas? bugs? or just say hi!)."]
 
-    [:h3 "Amplify"]
-    [:p "After you spend two minutes on a page, ampie will show you the amplify dialog "
-     "at the bottom of the screen."]
-    [:img.amplify {:src (.. browser -runtime (getURL "assets/images/amplify-dialog.png"))}]
-    [:p "The idea is to let you share the pages that are interesting more easily. "
-     "There is no expectation that you have read the page, or that you agree with it. "
-     "Ideally you amplify the pages that you might want to return back to months or years later. "
-     "After clicking amplify, you can add a comment and a reaction."]
-    [:img.amplify-edit {:src (.. browser -runtime (getURL "assets/images/amplify-dialog-edit.png"))}]
-    [:p "You can disable this dialog by going into the extension's settings. "]
-    [:p "I will add some more useful features related to this, "
-     "e.g. archiving the pages you amplify, or getting notified "
-     "when someone you follow leaves a comment on a page you have marked as to read. "
-     "Any suggestions how to make this more useful welcome!"]
-    [:p "You can change the shortcut to amplify the page in "
-     [:a (b/ahref-opts "chrome://extensions/shortcuts") "settings"]
-     "."]
+    [:p.pt-4 "That's enough to get you started, hope you like it!"]
 
-    [:h3 "Badges"]
-    [:p "Near some links ampie will display little ampersands (badges), they mark "
-     "the links that someone has amplified / tweeted / posted on HN. "
-     "Click on the badge to see more info about the link. "
-     "For any particular URL the badge is displayed at most three times "
-     "and is not shown after that to prevent visual clutter."]
-
-    #_[:h3 "Weekly links"]
-    #_[:p "Every Sunday ampie will offer you to choose the “weekly links” — "
-       "the best links you have found last week. "
-       "They don't have to be text posts: "
-       "anything goes, as long as you "
-       "find it interesting, important, and you might want to revisit it months or years later. "]
-
-    [:h3 "Feedback"]
-    [:p "Your feedback will help me a lot to make ampie better. "
-     "You can DM me your thoughts ("
-     [:a (b/ahref-opts "https://twitter.com/posobin") "@posobin"] "), or click "
-     "on the ampie icon in the extensions toolbar and click \"give feedback\" "
-     "to fill out a google form. I'll get an email with your feedback and will "
-     "definitely read it and respond."]
-
-    [:h3 "What now?"]
-    [:p "Click on the ampie icon in the toolbar to keep track of whether it is "
-     "done downloading and unpacking caches. "]
-    [:p "Hope you find ampie useful!"]]])
+    [:p.pt-2 "& "
+     [:a.text-underline (b/ahref-opts "https://twitter.com/posobin") "posobin"] " &"]]])
 
 (defn ^:dev/after-load init []
   (mount/start (mount/only #{#'ampie.background.backend/auth-token
--- src/main/ampie/pages/update.cljs
+++ src/main/ampie/pages/update.cljs
@@ -13,55 +13,37 @@
 
 (defn highlight [text bad?]
   [:span.rounded.pl-0dot5.pr-0dot5
-   {:class (if bad? :bg-indigo-200 :bg-green-300)}
+   {:class (if bad? :bg-indigo-200 :bg-yellow-200)}
    text])
 
 (defn update-page []
   [:div.font-sans.text-lg.leading-snug.ml-auto.mr-auto.w-fit-content.p-4.rounded-xl.shadow-xl.m-4.border.relative
    [:h1.text-2xl.font-italic.pb-3 "Ampie updated"]
    [:div.max-w-md
-    [:p "Updates to ampie, "
-     [highlight "with privacy implications for you." true]]
+    [:p "Hello!"]
+    [subheader "Sidebar opens twice per domain"]
+    [:p "Previously, if a domain had interesting links, sidebar would show up on every page. "
+     "Now it shows up " [highlight "twice per domain" false]
+     ", except for pages that have their own context to show. "
+     "For those pages sidebar would also be shown only on the first two visits. "
+     "If you still want to bring the sidebar up, press Alt-Alt."]
 
-    [subheader "Sidebar"]
-    [:div
-     [:p "Ampie now shows a sidebar on every page it has context for. "
-      [highlight
-       "Every URL you visit will be sent to ampie's server" true]
-      " to check the context. "
-      "You can disable it on some domains in settings "
-      [:span.tracking-tightest "——————————→"]]
-     [:div.absolute.-right-4.top-0
-      [:div.absolute.right-0.top-0.transform.translate-x-full
-       [:img.border.rounded-md.shadow.max-w-none.transform.scale-75.origin-top-left
-        {:src (.. browser -runtime (getURL "assets/images/settings-menu.png"))}]]]]
-    [:p.pt-2
-     [highlight "In return sidebar becomes more useful." false]
-     " I encourage you to try it out on some blog posts you like first. "
-     "Please reach out if you stop using ampie because of this."]
+    [subheader "Simpler close/open shortcut"]
+    [:p "Now you can "
+     [highlight "press Alt-Alt" false]
+     " to close/open the sidebar, instead of Alt-Shift - Alt-Shift."]
 
-    [subheader "Bye badges =("]
-    [:p "Many people told me how distracting badges can be."]
-    [:p "Not anymore."]
+    [subheader "Feedback form"]
+    [:p "If you want to give any thoughts about ampie (or anything else) "
+     "you can "
+     [highlight "scroll to the bottom of the sidebar" false]
+     " and easily reach me. "
+     "Looking forward to hearing from you =)"]
 
-    [subheader "Ampie in search results"]
-    [:p "No more ampie search results on google and DDG. "]
-    [:p "But now you will see "
-     [highlight "page context previews in search results" false]
-     " for google and ddg:"]
-    [:img.border.rounded-md.mt-3.in-search-results-half-width
-     {:src (.. browser -runtime (getURL "assets/images/in-search-results.png"))}]
-    [:p.pt-3 "This requires " [highlight "sending every search result URL to ampie's server" true]
-     ". Again, please tell me if you disable ampie because of this!"]
+    [:p.pt-4 "Tell me what you think (through the form or any other way)!"]
 
-    [:p.pt-6 "Overall, these changes aim to "
-     [highlight "simplify ampie and focus on what it does well." false]
-     " "
-     "I have more ideas for MVPs that build on what I already have, "
-     "and will iterate until people start using one of them."]
-    [:p.pt-4 "Tell me what you think!"]
-
-    [:p.pt-2 [:a.text-underline (b/ahref-opts "https://twitter.com/posobin") "@posobin"]]]])
+    [:p.pt-2 "& "
+     [:a.text-underline (b/ahref-opts "https://twitter.com/posobin") "posobin"] " &"]]])
 
 (defn ^:dev/after-load init []
   (rdom/render [update-page]
